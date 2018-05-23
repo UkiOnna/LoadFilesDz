46ec42ffe276a4a988bda92217f18d4b56ac224d
UkiOnna <sikirovtemirlan@mail.ru>
2018-05-23 17:01:12 +0600

    heh

--- UploadFileDz/LoadFile.cs
+++ UploadFileDz/LoadFile.cs
@@ -21,13 +21,12 @@ namespace UploadFileDz
             using (var dbx = new DropboxClient("1OHT5jM4DZYAAAAAAAAACjP94bicuOdxRJ41CBev9E5vK2B8nCX04bw4TJZyEnnN"))
             {
                 var full = await dbx.Users.GetCurrentAccountAsync();
-                Console.WriteLine($"Name - {full.Name.DisplayName} Email - {full.Email}");
+                //Console.WriteLine($"Name - {full.Name.DisplayName} Email - {full.Email}");
                 byte[] content = File.ReadAllBytes(Directory.GetCurrentDirectory() + @"\HelloUki.txt");
                 var memory = new MemoryStream(content);
                 var updated = await dbx.Files.UploadAsync(
                    "" + "/" + "PrIVEEEEETE.txt",
                    WriteMode.Overwrite.Instance, body: memory);
-
                 Console.WriteLine($"Saved   /Privki.txt rev {updated.Rev}");
             }
         }
