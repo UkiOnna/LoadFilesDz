04f2d162f3457a05cd0ca81602775f2d93143900
UkiOnna <sikirovtemirlan@mail.ru>
2018-05-23 16:54:46 +0600

    COM

---
+++ UploadFileDz/LoadFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Dropbox.Api;
+using Dropbox.Api.Files;
+
+namespace UploadFileDz
+{
+    public class LoadFile
+    {
+        public LoadFile()
+        {
+            var task = Task.Run(Go);
+        }
+
+        public async Task Go()
+        {
+            using (var dbx = new DropboxClient("1OHT5jM4DZYAAAAAAAAACjP94bicuOdxRJ41CBev9E5vK2B8nCX04bw4TJZyEnnN"))
+            {
+                var full = await dbx.Users.GetCurrentAccountAsync();
+                Console.WriteLine($"Name - {full.Name.DisplayName} Email - {full.Email}");
+                byte[] content = File.ReadAllBytes(Directory.GetCurrentDirectory() + @"\HelloUki.txt");
+                var memory = new MemoryStream(content);
+                var updated = await dbx.Files.UploadAsync(
+                   "" + "/" + "PrIVEEEEETE.txt",
+                   WriteMode.Overwrite.Instance, body: memory);
+
+                Console.WriteLine($"Saved   /Privki.txt rev {updated.Rev}");
+            }
+        }
+
+    }
+}
---
+++ UploadFileDz/Program.cs
@@ -0,0 +1,21 @@
+﻿using Dropbox.Api;
+using Dropbox.Api.Files;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UploadFileDz
+{
+    public class Program
+    {
+       static void Main(string[] args)
+        {
+            LoadFile file = new LoadFile();
+            Console.ReadLine();
+        }
+
+    }
+}
